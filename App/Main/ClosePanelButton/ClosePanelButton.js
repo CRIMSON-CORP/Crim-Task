8a0cc40dd2ff4959e5db5325f8cae5f6a7738c69
CRIMSON <olueatoworosanwo@gmail.com>
2022-03-17 14:02:59 +0100

    Made imgae splash screen and Init main App

---
+++ App/Main/ClosePanelButton/ClosePanelButton.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ClosePanelButton = () => {
+    return (
+        <View>
+            <Text>ClosePanelButton</Text>
+        </View>
+    );
+};
+
+export default ClosePanelButton;
