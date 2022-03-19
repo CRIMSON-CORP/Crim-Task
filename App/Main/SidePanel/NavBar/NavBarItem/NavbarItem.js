35b4960eeb5a22b3dd22fe909fcc3fd1efc94091
CRIMSON <olueatoworosanwo@gmail.com>
2022-03-19 12:39:05 +0100

    Added active state to Navitems

--- App/Main/SidePanel/NavBar/NavBarItem/NavbarItem.js
+++ App/Main/SidePanel/NavBar/NavBarItem/NavbarItem.js
@@ -1,20 +1,30 @@
 import { HStack, Text } from "native-base";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as ACTIONS from "../../../../../redux/ui/components/ui.actions";
 import AnimatedPressable from "../../../../Reusables/AnimatedPressable";
+import { View } from "moti";
 const NavbarItem = ({ icon, text, slug }) => {
     const dispath = useDispatch();
+    const ActiveNavItem = useSelector((state) => state.ui.view);
+    const Active = ActiveNavItem === slug;
     return (
-        <AnimatedPressable
-            onPress={() => dispath({ type: ACTIONS.SET_PANEL_VIEW, payload: { view: slug } })}
+        <View
+            animate={{
+                opacity: Active ? 1 : 0.6,
+                transform: [{ scale: Active ? 1 : 0.95 }],
+            }}
         >
-            <HStack space="8" alignItems="center">
-                {icon}
-                <Text fontSize={24} mt={-1}>
-                    {text}
-                </Text>
-            </HStack>
-        </AnimatedPressable>
+            <AnimatedPressable
+                onPress={() => dispath({ type: ACTIONS.SET_PANEL_VIEW, payload: { view: slug } })}
+            >
+                <HStack space="8" alignItems="center">
+                    {icon}
+                    <Text fontSize={24} mt={-1}>
+                        {text}
+                    </Text>
+                </HStack>
+            </AnimatedPressable>
+        </View>
     );
 };
 
