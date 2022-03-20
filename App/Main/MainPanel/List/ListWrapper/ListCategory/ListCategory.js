3562ed77f9989f0c0e96b7cca8063d6a496cd40d
CRIMSON <olueatoworosanwo@gmail.com>
2022-03-20 01:13:46 +0100

    used carousel animation library to make slider

--- App/Main/MainPanel/List/ListWrapper/ListCategory/ListCategory.js
+++ App/Main/MainPanel/List/ListWrapper/ListCategory/ListCategory.js
@@ -9,49 +9,34 @@ import Animated, {
     useSharedValue,
 } from "react-native-reanimated";
 import { Dimensions } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
 const { width } = Dimensions.get("window");
-const AnimatedHStack = Animated.createAnimatedComponent(HStack);
 const ListCategory = () => {
     const categories = useSelector((state) => state.tasks);
-    const AnimatedHStackSharedVlaue = useSharedValue(0);
-    const AnimatedHStackStartSharedVlaue = useSharedValue(0);
-    const gesture = useAnimatedGestureHandler({
-        onActive: (e) => {
-            const translation = e.translationX + AnimatedHStackStartSharedVlaue.value;
-            console.log(-((categories.length - 1) * (width * 0.6) + 80));
-            if (
-                translation <= 0 &&
-                translation >= -((categories.length - 1) * (width * 0.6) + 80)
-            ) {
-                AnimatedHStackSharedVlaue.value = translation;
-            }
-        },
-        onFinish: (e) => {
-            AnimatedHStackStartSharedVlaue.value = AnimatedHStackSharedVlaue.value;
-        },
-    });
-    const AnimatedHSTackStyle = useAnimatedStyle(() => ({
-        transform: [{ translateX: AnimatedHStackSharedVlaue.value }],
-    }));
     return (
         <VStack space={35}>
             <Text fontWeight={"bold"} opacity={0.7}>
                 Categories
             </Text>
-            <PanGestureHandler onGestureEvent={gesture}>
-                <AnimatedHStack style={AnimatedHSTackStyle}>
-                    {categories.map((item) => (
-                        <CategoryCardItem
-                            categoryColor={item.categoryColor}
-                            categoryTitle={item.categoryTitle}
-                            tasks={item.tasks}
-                            key={item.categoryId}
-                            mr="5"
-                            shadow="7"
-                        />
-                    ))}
-                </AnimatedHStack>
-            </PanGestureHandler>
+            <Carousel
+                width={width * 0.6 + 20}
+                loop={false}
+                showLength={0.5}
+                style={{
+                    overflow: "visible",
+                }}
+                data={categories}
+                renderItem={({ item }) => (
+                    <CategoryCardItem
+                        categoryColor={item.categoryColor}
+                        categoryTitle={item.categoryTitle}
+                        tasks={item.tasks}
+                        key={item.categoryId}
+                        mr="5"
+                        shadow="7"
+                    />
+                )}
+            />
         </VStack>
     );
 };
