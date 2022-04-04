36e57ce2be79b9e16daa565209f66e6c4b889be7
CRIMSON <olueatoworosanwo@gmail.com>
2022-04-05 00:59:52 +0100

    fixed splash screen and icon

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { loadAsync } from "expo-font";
-import { NativeBaseProvider } from "native-base";
-import { useEffect, useRef, useState } from "react";
+import { Box, NativeBaseProvider } from "native-base";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { enableScreens } from "react-native-screens";
 import { Provider } from "react-redux";
-import LoadingScreen from "./App/LoadingScreen";
 import Main from "./App/Main/Main";
 import Fonts from "./assets/fonts";
 import store from "./redux";
@@ -16,6 +15,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import EnteringScreen from "./App/EnteringScreen";
 import Onboarding from "./App/Onboarding";
 import CreateAccount from "./App/CreateAccount";
+import { getAsyncAccountData, getAsyncTaskData, getAsyncUIData } from "./utils/utils";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 enableScreens();
 const Stack = createStackNavigator();
 export default function App() {
@@ -32,10 +33,14 @@ function MainWrapper() {
     useEffect(() => {
         async function getData() {
             try {
+                await preventAutoHideAsync();
                 const AsyncData = await AsyncStorage.getItem("crim-task-data");
                 await loadAsync({ ...Fonts.Raleway, ...Fonts.Gisha });
                 if (AsyncData) {
                     setUserExist(true);
+                    getAsyncAccountData();
+                    getAsyncTaskData();
+                    getAsyncUIData();
                 } else {
                     setUserExist(false);
                 }
@@ -47,45 +52,52 @@ function MainWrapper() {
         }
         getData();
     }, []);
+    const onLayOutView = useCallback(async () => {
+        if (!Loading) {
+            await hideAsync();
+        }
+    }, [Loading]);
     return (
         <NavigationContainer theme={navigationCardTheme} ref={NavigationRef}>
             <NativeBaseProvider theme={theme}>
-                {Loading ? (
-                    <LoadingScreen />
-                ) : (
-                    <NavigationContext.Provider value={{ NavigationRef: NavigationRef.current }}>
-                        <GestureHandlerRootView style={{ flex: 1 }}>
-                            <AuthContext.Provider value={{ userExist, setUserExist }}>
-                                <Stack.Navigator
-                                    screenOptions={{
-                                        headerMode: "none",
-                                        cardStyle: {
-                                            overflow: "visible",
-                                        },
-                                    }}
-                                >
-                                    {userExist && <Stack.Screen name="main" component={Main} />}
-                                    {!userExist && (
-                                        <>
-                                            <Stack.Screen
-                                                name="entering"
-                                                component={EnteringScreen}
-                                            />
-                                            <Stack.Screen
-                                                name="onboarding"
-                                                component={Onboarding}
-                                            />
-                                            <Stack.Screen
-                                                name="createAcc"
-                                                component={CreateAccount}
-                                            />
-                                        </>
-                                    )}
-                                </Stack.Navigator>
-                            </AuthContext.Provider>
-                        </GestureHandlerRootView>
-                    </NavigationContext.Provider>
-                )}
+                {!Loading ? (
+                    <Box onLayout={onLayOutView} flex={1}>
+                        <NavigationContext.Provider
+                            value={{ NavigationRef: NavigationRef.current }}
+                        >
+                            <GestureHandlerRootView style={{ flex: 1 }}>
+                                <AuthContext.Provider value={{ userExist, setUserExist }}>
+                                    <Stack.Navigator
+                                        screenOptions={{
+                                            headerMode: "none",
+                                            cardStyle: {
+                                                overflow: "visible",
+                                            },
+                                        }}
+                                    >
+                                        {userExist && <Stack.Screen name="main" component={Main} />}
+                                        {!userExist && (
+                                            <>
+                                                <Stack.Screen
+                                                    name="entering"
+                                                    component={EnteringScreen}
+                                                />
+                                                <Stack.Screen
+                                                    name="onboarding"
+                                                    component={Onboarding}
+                                                />
+                                                <Stack.Screen
+                                                    name="createAcc"
+                                                    component={CreateAccount}
+                                                />
+                                            </>
+                                        )}
+                                    </Stack.Navigator>
+                                </AuthContext.Provider>
+                            </GestureHandlerRootView>
+                        </NavigationContext.Provider>
+                    </Box>
+                ) : null}
             </NativeBaseProvider>
         </NavigationContainer>
     );
--- App/Main/Main.js
+++ App/Main/Main.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Box } from "native-base";
 import SidePanel from "./SidePanel/SidePanel";
 import MainPanel from "./MainPanel/MainPanel";
-import { getAsyncAccountData, getAsyncTaskData, getAsyncUIData } from "../../utils/utils";
 function Main() {
-    getAsyncAccountData();
-    getAsyncTaskData();
-    getAsyncUIData();
     return (
         <Box flex={1} bg="primary.500">
             <SidePanel />
