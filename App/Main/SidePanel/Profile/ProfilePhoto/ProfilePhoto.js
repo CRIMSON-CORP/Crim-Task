08ec9f17805195fd300d21f7d8d4b8687c15ca3d
CRIMSON <olueatoworosanwo@gmail.com>
2022-04-07 17:46:17 +0100

    ADded SVg to profile picture

--- App/Main/SidePanel/Profile/ProfilePhoto/ProfilePhoto.js
+++ App/Main/SidePanel/Profile/ProfilePhoto/ProfilePhoto.js
@@ -1,43 +1,59 @@
-import { Center, Image, Box } from "native-base";
-import React, { useContext } from "react";
+import { Center, Image, Box, useTheme } from "native-base";
+import React, { useContext, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CLOSE_SIDE } from "../../../../../redux/ui/components/ui.actions";
 import { NavigationContext } from "../../../../../utils/context";
 import UserIcon from "../../../../Reusables/UserIcon/UserIcon";
 import AnimatedPressable from "../../../../Reusables/AnimatedPressable";
+import Svg, { Circle } from "react-native-svg";
 const ProfilePhoto = () => {
     const profile = useSelector((state) => state.account.profilePhoto);
     const { NavigationRef } = useContext(NavigationContext);
     const dispatch = useDispatch();
+    const { colors } = useTheme();
+
     return (
-        <Center size={115}>
+        <Center size={120}>
             <AnimatedPressable
                 onPress={() => {
                     NavigationRef.navigate("settings");
                     dispatch({ type: CLOSE_SIDE });
                 }}
                 style={{
-                    width: 108,
-                    height: 108,
+                    width: 120,
+                    height: 120,
                 }}
             >
                 <Center w="full" h="full">
-                    <Box
-                        w="full"
-                        h="full"
-                        borderColor={"#ffffff50"}
-                        position="absolute"
-                        rounded="full"
-                        borderWidth={2}
-                    />
+                    <Svg
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            position: "absolute",
+                            transform: [{ rotate: "30deg" }],
+                        }}
+                        viewBox="0 0 100 100"
+                    >
+                        <Circle cx="50" cy="50" r="49" stroke="#ffffff50" strokeWidth={2} />
+                        <Circle
+                            cx="50"
+                            cy="50"
+                            r="49"
+                            stroke={colors.primary[100]}
+                            strokeDasharray={307.3787841796875}
+                            strokeDashoffset={250}
+                            strokeWidth={3}
+                            strokeLinecap="round"
+                        />
+                    </Svg>
                     {profile ? (
                         <Image
                             rounded="full"
                             source={{
                                 uri: `data:image/*;base64,${profile.uri}`,
                             }}
-                            w={108}
-                            h={108}
+                            w={110}
+                            h={110}
                             resizeMode="cover"
                             alt="profile"
                         />
