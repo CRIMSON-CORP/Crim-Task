ec713066fc1fd00b857103b22d65ccc52b213677
CRIMSON <olueatoworosanwo@gmail.com>
2022-04-08 02:56:41 +0100

    removed scrollbar for create task

--- App/Main/MainPanel/Fab/CreateNewTask/CreateNewTask.js
+++ App/Main/MainPanel/Fab/CreateNewTask/CreateNewTask.js
@@ -6,7 +6,6 @@ import { CREATE_CATEGORY_TASK } from "../../../../../redux/tasks/components/task
 import FabCTA from "../FabCTA";
 import AnimatedText from "../../../../Reusables/AnimatedText/AnimatedText";
 import { View as MotiView } from "moti";
-import { ScrollView } from "react-native-gesture-handler";
 function CreateNewTask() {
     const state_categories = useSelector((state) => state.tasks);
     const categories = state_categories.map((cat) => ({
@@ -23,89 +22,82 @@ function CreateNewTask() {
         setActiveIndexIndicator(categories.findIndex((cat) => cat.categoryId === ActiveCategoryId));
     }, [ActiveCategoryId]);
     return (
-        <ScrollView
-            contentContainerStyle={{
-                flexGrow: 1,
-                justifyContent: "center",
-            }}
-        >
-            <VStack w="full" space={35}>
-                <VStack w="full">
-                    <Box w={"80%"}>
-                        <AnimatedText text="Create a new Task" />
-                    </Box>
-                </VStack>
-                <VStack space="30">
-                    <Text fontSize="sm" opacity={0.7}>
-                        Subject
-                    </Text>
-                    <Input
-                        size="xl"
-                        variant="underlined"
-                        color="white"
-                        isFullWidth
-                        selectionColor="white"
-                        borderBottomColor="white"
-                        underlineColorAndroid={"transparent"}
-                        maxLength={35}
-                        numberOfLines={1}
-                        px="5"
-                        value={subject}
-                        onChangeText={(text) => setSubject(text)}
-                    />
-                </VStack>
-                <VStack space="5">
-                    <Text fontSize="sm" opacity={0.7}>
-                        Task Category
-                    </Text>
-                    <Box px={1} flexWrap="wrap" flexDirection="row" ref={pillContainerRef}>
-                        <MotiView
-                            style={{
-                                borderRadius: 15,
-                                borderWidth: 2,
-                                borderColor: "white",
-                                position: "absolute",
-                            }}
-                            animate={{
-                                width: pillsDimensions[ActiveIndexIndicator]?.width || 50,
-                                height: pillsDimensions[ActiveIndexIndicator]?.height || 50,
-                                left: pillsDimensions[ActiveIndexIndicator]?.pageX || 0,
-                                top: pillsDimensions[ActiveIndexIndicator]?.pageY || 0,
-                            }}
-                            transition={{
-                                type: "spring",
-                                damping: 13,
-                            }}
-                        />
-                        {categories.map(({ categoryTitle, categoryId }) => (
-                            <Pill
-                                key={categoryId}
-                                categoryId={categoryId}
-                                categoryTitle={categoryTitle}
-                                setActiveCategoryId={setActiveCategoryId}
-                                setPillsDimensions={setPillsDimensions}
-                                containerRef={pillContainerRef}
-                            />
-                        ))}
-                    </Box>
-                </VStack>
-                <FabCTA
-                    title="Create New Task"
-                    onClick={
-                        subject &&
-                        (() => {
-                            dispatch({
-                                type: CREATE_CATEGORY_TASK,
-                                payload: {
-                                    subject: subject.trim(),
-                                    categoryId: ActiveCategoryId,
-                                },
-                            });
-                        })
-                    }
+        <VStack w="full" space={35}>
+            <VStack w="full">
+                <Box w={"80%"}>
+                    <AnimatedText text="Create a new Task" />
+                </Box>
+            </VStack>
+            <VStack space="30">
+                <Text fontSize="sm" opacity={0.7}>
+                    Subject
+                </Text>
+                <Input
+                    size="xl"
+                    variant="underlined"
+                    color="white"
+                    isFullWidth
+                    selectionColor="white"
+                    borderBottomColor="white"
+                    underlineColorAndroid={"transparent"}
+                    maxLength={35}
+                    numberOfLines={1}
+                    px="5"
+                    value={subject}
+                    onChangeText={(text) => setSubject(text)}
                 />
             </VStack>
-        </ScrollView>
+            <VStack space="5">
+                <Text fontSize="sm" opacity={0.7}>
+                    Task Category
+                </Text>
+                <Box px={1} flexWrap="wrap" flexDirection="row" ref={pillContainerRef}>
+                    <MotiView
+                        style={{
+                            borderRadius: 15,
+                            borderWidth: 2,
+                            borderColor: "white",
+                            position: "absolute",
+                        }}
+                        animate={{
+                            width: pillsDimensions[ActiveIndexIndicator]?.width || 50,
+                            height: pillsDimensions[ActiveIndexIndicator]?.height || 50,
+                            left: pillsDimensions[ActiveIndexIndicator]?.pageX || 0,
+                            top: pillsDimensions[ActiveIndexIndicator]?.pageY || 0,
+                        }}
+                        transition={{
+                            type: "spring",
+                            damping: 13,
+                        }}
+                    />
+                    {categories.map(({ categoryTitle, categoryId }) => (
+                        <Pill
+                            key={categoryId}
+                            categoryId={categoryId}
+                            categoryTitle={categoryTitle}
+                            setActiveCategoryId={setActiveCategoryId}
+                            setPillsDimensions={setPillsDimensions}
+                            containerRef={pillContainerRef}
+                        />
+                    ))}
+                </Box>
+            </VStack>
+            <FabCTA
+                title="Create New Task"
+                onClick={
+                    subject &&
+                    (() => {
+                        dispatch({
+                            type: CREATE_CATEGORY_TASK,
+                            payload: {
+                                subject: subject.trim(),
+                                categoryId: ActiveCategoryId,
+                            },
+                        });
+                    })
+                }
+            />
+        </VStack>
     );
 }
 
