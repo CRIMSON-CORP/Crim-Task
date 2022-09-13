567f2bbf4f88f9068afcd6a4319af9e44307e65a
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-13 23:07:01 +0100

    refactor navbaritem

--- App/Main/SidePanel/NavBar/NavBarItem/NavbarItem.js
+++ App/Main/SidePanel/NavBar/NavBarItem/NavbarItem.js
@@ -1,16 +1,27 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { View } from "moti";
 import { HStack, Text } from "native-base";
-import { useContext, useEffect, useState } from "react";
-import { NavigationContext } from "../../../../../utils/context";
+import { useEffect, useState } from "react";
 import AnimatedPressable from "../../../../Reusables/AnimatedPressable";
 import { useSidePanel } from "../../../../../utils/contexts/sidePanelOpenedContext";
 import { useNavigation } from "../../../../../utils/contexts/navigationContext";
+import { useCallback } from "react";
+import { useMemo } from "react";
 
+/**
+ * @typedef NavbarItemParams
+ * @property {JSX.Element} icon
+ * @property {string} text
+ * @property {string} slug
+ */
+
+/**
+ * @param {NavbarItemParams} NavBarItemParams
+ * @returns {JSX.Element}
+ */
 function NavbarItem({ icon, text, slug }) {
     const { NavigationRef } = useNavigation();
-    const [Active, setActive] = useState(false);
+    const [active, setActive] = useState(false);
     const { setSidePanelOpened } = useSidePanel();
     useEffect(() => {
         const unsub = NavigationRef.addListener("state", () => {
@@ -19,19 +30,22 @@ function NavbarItem({ icon, text, slug }) {
 
         return unsub;
     }, []);
+
+    const navigateToScreen = useCallback(() => {
+        NavigationRef.navigate(slug);
+        setSidePanelOpened(false);
+    }, [slug]);
+
+    const activeState = useMemo(
+        () => ({
+            opacity: active ? 1 : 0.6,
+            transform: [{ scale: active ? 1 : 0.95 }],
+        }),
+        [active]
+    );
     return (
-        <View
-            animate={{
-                opacity: Active ? 1 : 0.6,
-                transform: [{ scale: Active ? 1 : 0.95 }],
-            }}
-        >
-            <AnimatedPressable
-                onPress={() => {
-                    NavigationRef.navigate(slug);
-                    setSidePanelOpened(false);
-                }}
-            >
+        <View animate={activeState}>
+            <AnimatedPressable onPress={navigateToScreen}>
                 <HStack space="8" alignItems="center">
                     {icon}
                     <Text fontSize={24} mt={-1}>
