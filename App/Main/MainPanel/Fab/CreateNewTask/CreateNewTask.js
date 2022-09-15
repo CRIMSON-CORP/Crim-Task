0c2e905843cb16317feebae47d970543c0d7541e
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-15 17:50:42 +0100

    validate create new task

--- App/Main/MainPanel/Fab/CreateNewTask/CreateNewTask.js
+++ App/Main/MainPanel/Fab/CreateNewTask/CreateNewTask.js
@@ -1,20 +1,21 @@
 import { View as MotiView } from "moti";
+import PropTypes from "prop-types";
 import { Box, Input, Text, useTheme, VStack } from "native-base";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useMemo, useCallback } from "react";
+import { StyleSheet } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import {
-    CREATE_CATEGORY_TASK,
-    EDIT_TASK,
-} from "../../../../../redux/tasks/components/task.actions";
 import AnimatedPressable from "../../../../Reusables/AnimatedPressable";
 import AnimatedText from "../../../../Reusables/AnimatedText/AnimatedText";
 import FabCTA from "../FabCTA";
+import KeyboardViewAdjuster from "../../../../Reusables/KeyboardViewAdjuster";
+import { createTask } from "../../../../../redux/tasks/components/task.reducer";
+
+const indicatorTransition = {
+    type: "spring",
+    damping: 13,
+};
 function CreateNewTask({ flag }) {
-    const state_categories = useSelector((state) => state.tasks);
-    const categories = state_categories.map((cat) => ({
-        categoryId: cat.categoryId,
-        categoryTitle: cat.categoryTitle,
-    }));
+    const categories = useSelector((state) => state.tasks);
     const [ActiveCategoryId, setActiveCategoryId] = useState(
         flag
             ? categories.find((cat) => cat.categoryId === flag.currentCategoryId).categoryId
@@ -28,90 +29,98 @@ function CreateNewTask({ flag }) {
     const { colors } = useTheme();
     const dispatch = useDispatch();
     const pillContainerRef = useRef();
+
+    const indicatorAnimatedStyle = useMemo(
+        () => ({
+            width: pillsDimensions[ActiveIndexIndicator]?.width || 38,
+            height: pillsDimensions[ActiveIndexIndicator]?.height || 38,
+            left: pillsDimensions[ActiveIndexIndicator]?.pageX || 0,
+            top: pillsDimensions[ActiveIndexIndicator]?.pageY || 0,
+        }),
+        [pillsDimensions, ActiveIndexIndicator]
+    );
+
+    const _createTask = useCallback(() => {
+        let commonProperties = {
+            subject: subject.trim(),
+            categoryId: ActiveCategoryId,
+        };
+        if (flag) {
+            dispatch(
+                createTask({
+                    ...commonProperties,
+                    itemId: flag.itemId,
+                    currentCategoryId: flag.currentCategoryId,
+                })
+            );
+        }
+        dispatch(createTask(commonProperties));
+    }, [flag, subject, ActiveCategoryId]);
+
     useEffect(() => {
         setActiveIndexIndicator(categories.findIndex((cat) => cat.categoryId === ActiveCategoryId));
     }, [ActiveCategoryId]);
+
+    const PAGE_TITLE = flag ? "Edit Task" : "Create a new Task";
+    const PAGE_CATEGORY_SUBTITLE = flag ? "Move to another Category" : "Select Task Category";
+    const CTA_TEXT = flag ? "Edit Task" : "Create New Task";
+
     return (
-        <VStack w="full" space={35}>
-            <VStack w="full">
-                <Box w={"80%"}>
-                    <AnimatedText text={`${flag ? "Edit Task" : "Create a new Task"}`} />
-                </Box>
-            </VStack>
-            <VStack space="30">
-                <Text fontSize="sm" opacity={0.7}>
-                    Subject
-                </Text>
-                <Input
-                    size="xl"
-                    variant="underlined"
-                    color="white"
-                    isFullWidth
-                    selectionColor={colors.primary.accent}
-                    borderBottomColor="white"
-                    underlineColorAndroid={"transparent"}
-                    px="5"
-                    value={subject}
-                    onChangeText={(text) => setSubject(text)}
-                />
-            </VStack>
-            <VStack space="5">
-                <Text fontSize="sm" opacity={0.7}>
-                    {flag ? "Move to another Category" : "Select Task Category"}
-                </Text>
-                <Box px={1} flexWrap="wrap" flexDirection="row" ref={pillContainerRef}>
-                    <MotiView
-                        style={{
-                            borderRadius: 15,
-                            borderWidth: 2,
-                            borderColor: "white",
-                            position: "absolute",
-                        }}
-                        animate={{
-                            width: pillsDimensions[ActiveIndexIndicator]?.width || 38,
-                            height: pillsDimensions[ActiveIndexIndicator]?.height || 38,
-                            left: pillsDimensions[ActiveIndexIndicator]?.pageX || 0,
-                            top: pillsDimensions[ActiveIndexIndicator]?.pageY || 0,
-                        }}
-                        transition={{
-                            type: "spring",
-                            damping: 13,
-                        }}
+        <KeyboardViewAdjuster>
+            <VStack w="full" space={35}>
+                <VStack w="full">
+                    <Box w={"80%"}>
+                        <AnimatedText text={PAGE_TITLE} />
+                    </Box>
+                </VStack>
+                <VStack space="30">
+                    <Text fontSize="sm" opacity={0.7}>
+                        Subject
+                    </Text>
+                    <Input
+                        px="5"
+                        size="xl"
+                        isFullWidth
+                        color="white"
+                        value={subject}
+                        variant="underlined"
+                        borderBottomColor="white"
+                        selectionColor={colors.primary.accent}
+                        underlineColorAndroid={"transparent"}
+                        onChangeText={(text) => setSubject(text)}
                     />
-                    {categories.map(({ categoryTitle, categoryId }) => (
-                        <Pill
-                            key={categoryId}
-                            categoryId={categoryId}
-                            categoryTitle={categoryTitle}
-                            setActiveCategoryId={setActiveCategoryId}
-                            setPillsDimensions={setPillsDimensions}
-                            containerRef={pillContainerRef}
+                </VStack>
+                <VStack space="5">
+                    <Text fontSize="sm" opacity={0.7}>
+                        {PAGE_CATEGORY_SUBTITLE}
+                    </Text>
+                    <Box px={1} flexWrap="wrap" flexDirection="row" ref={pillContainerRef}>
+                        <MotiView
+                            style={styles.selectCategoryIndicator}
+                            animate={indicatorAnimatedStyle}
+                            transition={indicatorTransition}
                         />
-                    ))}
-                </Box>
+                        {categories.map(({ categoryTitle, categoryId }) => (
+                            <Pill
+                                key={categoryId}
+                                categoryId={categoryId}
+                                categoryTitle={categoryTitle}
+                                setActiveCategoryId={setActiveCategoryId}
+                                setPillsDimensions={setPillsDimensions}
+                                containerRef={pillContainerRef}
+                            />
+                        ))}
+                    </Box>
+                </VStack>
+                <FabCTA title={CTA_TEXT} onClick={subject && _createTask} />
             </VStack>
-            <FabCTA
-                title={`${flag ? "Edit Task" : "Create New Task"}`}
-                onClick={
-                    subject &&
-                    (() => {
-                        dispatch({
-                            type: flag ? EDIT_TASK : CREATE_CATEGORY_TASK,
-                            payload: {
-                                subject: subject.trim(),
-                                categoryId: ActiveCategoryId,
-                                ...(flag && {
-                                    itemId: flag.itemId,
-                                    currentCategoryId: flag.currentCategoryId,
-                                }),
-                            },
-                        });
-                    })
-                }
-            />
-        </VStack>
+        </KeyboardViewAdjuster>
     );
 }
+
+CreateNewTask.propTypes = {
+    flag: PropTypes.object,
+};
 
 export default CreateNewTask;
 
@@ -131,17 +140,7 @@ function Pill({
     }, []);
     return (
         <AnimatedPressable onPress={() => setActiveCategoryId(categoryId)}>
-            <MotiView
-                style={{
-                    backgroundColor: "#ffffff20",
-                    paddingHorizontal: 16,
-                    paddingVertical: 10,
-                    marginRight: 16,
-                    borderRadius: 15,
-                    marginBottom: 15,
-                }}
-                ref={pillRef}
-            >
+            <MotiView ref={pillRef} style={styles.pillStyles}>
                 <Text fontWeight="600" lineHeight={18}>
                     {categoryTitle}
                 </Text>
@@ -149,3 +148,28 @@ function Pill({
         </AnimatedPressable>
     );
 }
+
+Pill.propTypes = {
+    categoryId: PropTypes.string,
+    categoryTitle: PropTypes.string,
+    setActiveCategoryId: PropTypes.string,
+    setPillsDimensions: PropTypes.func,
+    containerRef: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+    selectCategoryIndicator: {
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: "white",
+        position: "absolute",
+    },
+    pillStyles: {
+        backgroundColor: "#ffffff20",
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+        marginRight: 16,
+        borderRadius: 15,
+        marginBottom: 15,
+    },
+});
