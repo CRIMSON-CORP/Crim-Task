c814b617c7959c22e0389609423350564202a2b1
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-16 16:34:43 +0100

    validate task item

--- App/Reusables/TaskItem/TaskItem.js
+++ App/Reusables/TaskItem/TaskItem.js
@@ -1,17 +1,16 @@
 import { MotiView } from "moti";
 import { HStack, Pressable } from "native-base";
 import PropTypes from "prop-types";
-import { memo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import { Layout } from "react-native-reanimated";
 import { useDispatch } from "react-redux";
-import { DELETE_TASK, UPDATE_TASK } from "../../../redux/tasks/components/task.actions";
 import { deleteTask, updateTask } from "../../../redux/tasks/components/task.reducer";
-import { useFab } from "../../../utils/context";
-import AnimatedTaskText from "../AnimatedTaaskText/AnimatedTaskText";
+import { useFab } from "../../../utils/contexts/fabContext";
+import AnimatedTaskText from "../AnimatedTaskText";
 import SwipableView from "../SwipableView";
 import AnimatedCheckBox from "./AnimatedCheckBox/";
 
-const TaskItem = ({
+function TaskItem({
     task,
     completed,
     categoryColor,
@@ -19,55 +18,55 @@ const TaskItem = ({
     itemId,
     dark,
     simultaneousHandlers,
-}) => {
-    // const dispath = useDispatch();
-    const { setFlag, ToggleOpenFab } = useFab();
+}) {
+    const dispatch = useDispatch();
+    const { setFlag, setFabPanelOpen } = useFab();
+
+    const _deleteTask = useCallback(() => {
+        dispatch(
+            deleteTask({
+                categoryId,
+                itemId,
+            })
+        );
+    }, [categoryId, itemId]);
+
+    const openEditScreen = useCallback(() => {
+        setFlag({
+            flag: "EDIT_TASK",
+            subject: task,
+            currentCategoryId: categoryId,
+            itemId,
+        });
+        setFabPanelOpen(true);
+    }, [task, categoryId, itemId]);
+
+    const _updateTask = useCallback(() => {
+        dispatch(
+            updateTask({
+                categoryId,
+                itemId,
+            })
+        );
+    }, [categoryId, itemId]);
+
+    const StackStyles = useMemo(
+        () => ({
+            p: "5",
+            bg: dark ? "primary.400" : "primary.300",
+            shadow: "5",
+            space: "15",
+            rounded: "15",
+            alignItems: "center",
+        }),
+        [dark]
+    );
+
     return (
         <MotiView layout={Layout.springify()}>
-            <SwipableView
-                swipeExe={
-                    () =>
-                        deleteTask({
-                            categoryId,
-                            itemId,
-                        })
-                    // dispath({
-                    //     type: DELETE_TASK,
-                    //     payload: ,
-                    // })
-                }
-                simultaneousHandlers={simultaneousHandlers}
-            >
-                <Pressable
-                    onLongPress={() => {
-                        setFlag({
-                            flag: "EDIT_TASK",
-                            subject: task,
-                            currentCategoryId: categoryId,
-                            itemId,
-                        });
-                        ToggleOpenFab(true);
-                    }}
-                    onPress={() => {
-                        updateTask({
-                            categoryId,
-                            itemId,
-                        });
-                        // dispath({
-                        //     type: UPDATE_TASK,
-                        //     payload: ,
-                        // });
-                    }}
-                    mb={30}
-                >
-                    <HStack
-                        alignItems={"center"}
-                        p="5"
-                        bg={dark ? "primary.400" : "primary.300"}
-                        rounded="15"
-                        space="15"
-                        shadow="5"
-                    >
+            <SwipableView swipeExe={_deleteTask} simultaneousHandlers={simultaneousHandlers}>
+                <Pressable onLongPress={openEditScreen} onPress={_updateTask} mb={30}>
+                    <HStack {...StackStyles}>
                         <AnimatedCheckBox completed={completed} color={categoryColor} />
                         <AnimatedTaskText task={task} completed={completed} />
                     </HStack>
@@ -75,7 +74,7 @@ const TaskItem = ({
             </SwipableView>
         </MotiView>
     );
-};
+}
 
 TaskItem.propTypes = {
     itemId: PropTypes.string.isRequired,
@@ -84,5 +83,6 @@ TaskItem.propTypes = {
     categoryColor: PropTypes.string.isRequired,
     categoryId: PropTypes.string.isRequired,
     dark: PropTypes.bool,
+    simultaneousHandlers: PropTypes.object,
 };
 export default memo(TaskItem);
