1118bd582c1888f5569a8bdb9b76842eb6c9089d
CRIMSON <olueatoworosanwo@gmail.com>
2022-09-17 15:49:09 +0100

    changes to fab

--- utils/contexts/fabContext.js
+++ utils/contexts/fabContext.js
@@ -63,10 +63,10 @@ export function FabContextProvider({ children }) {
     function setFabPanelOpen(state) {
         if (state) {
             setAnimateOpen(state);
-            setTimeout(() => _setFabPanelOpen(state), 100);
+            setTimeout(() => _setFabPanelOpen(state), 0);
         } else {
             _setFabPanelOpen(state);
-            setTimeout(() => setAnimateOpen(state), 100);
+            setTimeout(() => setAnimateOpen(state), 0);
         }
     }
 
