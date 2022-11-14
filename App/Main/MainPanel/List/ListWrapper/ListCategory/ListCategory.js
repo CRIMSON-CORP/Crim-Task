744851ec1a16396e2b480fa7859791ecc454b8f8
CRIMSON <olueatoworosanwo@gmail.com>
2022-11-14 04:41:56 +0100

    fix too much space under category list

--- App/Main/MainPanel/List/ListWrapper/ListCategory/ListCategory.js
+++ App/Main/MainPanel/List/ListWrapper/ListCategory/ListCategory.js
@@ -1,5 +1,5 @@
 import { AnimatePresence, MotiView } from "moti";
-import { Image, Text, VStack } from "native-base";
+import { Box, Image, Text, VStack } from "native-base";
 import { Dimensions, StyleSheet } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
 import { useSelector } from "react-redux";
@@ -34,35 +34,40 @@ function ListCategory() {
             <Text fontWeight="bold" opacity={0.7}>
                 Categories
             </Text>
-            {categories.length !== 0 && (
-                <Carousel
-                    height={120}
-                    loop={false}
-                    defaultIndex={0}
-                    style={styles.carousel}
-                    width={width * 0.6 + 20}
-                    scrollAnimationDuration={500}
-                    data={[...categories.map((item) => JSON.stringify(item))]}
-                    renderItem={({ item }) => <CategoryCardItem {...JSON.parse(item)} shadow="7" />}
-                />
-            )}
-            <AnimatePresence>
-                {categories.length === 0 && (
-                    <MotiView {...noTaskImageAnimationConfig}>
-                        <VStack justifyContent="flex-start" h={120} p={5} space="2">
-                            <Image
-                                h="full"
-                                alt="idle category"
-                                resizeMode="contain"
-                                source={IdleCategory}
-                            />
-                            <Text textAlign={"center"} fontSize={10}>
-                                You have no Categories, press the "+" button to add a new Category
-                            </Text>
-                        </VStack>
-                    </MotiView>
+            <Box>
+                {categories.length !== 0 && (
+                    <Carousel
+                        height={120}
+                        loop={false}
+                        defaultIndex={0}
+                        style={styles.carousel}
+                        width={width * 0.6 + 20}
+                        scrollAnimationDuration={500}
+                        data={[...categories.map((item) => JSON.stringify(item))]}
+                        renderItem={({ item }) => (
+                            <CategoryCardItem {...JSON.parse(item)} shadow="7" />
+                        )}
+                    />
                 )}
-            </AnimatePresence>
+                <AnimatePresence>
+                    {categories.length === 0 && (
+                        <MotiView {...noTaskImageAnimationConfig}>
+                            <VStack justifyContent="flex-start" h={120} p={5} space="2">
+                                <Image
+                                    h="full"
+                                    alt="idle category"
+                                    resizeMode="contain"
+                                    source={IdleCategory}
+                                />
+                                <Text textAlign={"center"} fontSize={10}>
+                                    You have no Categories, press the "+" button to add a new
+                                    Category
+                                </Text>
+                            </VStack>
+                        </MotiView>
+                    )}
+                </AnimatePresence>
+            </Box>
         </VStack>
     );
 }
